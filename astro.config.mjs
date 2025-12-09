66038fcbb7a02317e4ee4234d25c473467bdc28e
exuseric <ericbro29@gmail.com>
2025-12-09 17:52:46 +0300

    update: install tailwind, react and react-aria-components

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,36 +4,45 @@ import netlify from "@astrojs/netlify"
 import sitemap from "@astrojs/sitemap"
 import purgecss from "astro-purgecss"
 
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ericmaina.me",
-    build: {
-        inlineStylesheets: "never",
-    },
-    adapter: netlify({
-        imageCDN: false,
-        functionPerRoute: false,
-        _redirects: [
-            // Security headers
-            "/*    /.netlify/functions/entry    200",
-            // Redirect www to non-www
-            "https://www.ericmaina.me/* https://ericmaina.me/:splat 301!",
-            // Force HTTPS
-            "http://ericmaina.me/* https://ericmaina.me/:splat 301!",
-        ],
-    }),
-    image: {
-        responsiveStyles: true,
-        layout: "constrained",
-        domains: ["eu-west-2.graphassets.com"],
-    },
-    integrations: [
-        sitemap(),
-        purgecss({
-            variables: true,
-            keyframes: true,
-            fontFace: true,
-            safelist: [/^btn--/],
-        }),
-    ],
+  site: "https://ericmaina.me",
+
+  build: {
+      inlineStylesheets: "never",
+  },
+
+  adapter: netlify({
+      imageCDN: false,
+      functionPerRoute: false,
+      _redirects: [
+          // Security headers
+          "/*    /.netlify/functions/entry    200",
+          // Redirect www to non-www
+          "https://www.ericmaina.me/* https://ericmaina.me/:splat 301!",
+          // Force HTTPS
+          "http://ericmaina.me/* https://ericmaina.me/:splat 301!",
+      ],
+  }),
+
+  image: {
+      responsiveStyles: true,
+      layout: "constrained",
+      domains: ["eu-west-2.graphassets.com"],
+  },
+
+  integrations: [sitemap(), purgecss({
+      variables: true,
+      keyframes: true,
+      fontFace: true,
+      safelist: [/^btn--/],
+  }), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
