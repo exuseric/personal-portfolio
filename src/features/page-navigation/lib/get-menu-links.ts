c4586d4b3ec55d2ca9a7d9745a08bcc8ec5bd236
exuseric <ericbro29@gmail.com>
2026-02-14 16:50:29 +0300

    refactor(navigation): remove desktop and mobile components
    - Removed `DesktopNavigation` and `MobileNavigation` components, along with their imports and usage.
    - Updated `pnpm-lock.yaml` to reflect dependency changes, potentially related to removing unused packages and configurations.

---
+++ src/features/page-navigation/lib/get-menu-links.ts
@@ -0,0 +1,26 @@
+export const links = [
+    // {
+    //     label: "Home",
+    //     url: "/",
+    //     type: "internal",
+    //     icon: "hugeicons:home-01",
+    // },
+    {
+        label: "Projects",
+        url: "#projects",
+        type: "internal",
+        icon: "hugeicons:code-folder",
+    },
+    {
+        label: "Contact",
+        url: "#contact",
+        type: "internal",
+        icon: "hugeicons:message-01",
+    },
+    {
+        label: "Q&A",
+        url: "#faq",
+        type: "internal",
+        icon: "hugeicons:help-square",
+    },
+]
